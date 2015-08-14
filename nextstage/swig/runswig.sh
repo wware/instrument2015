5aa53660d2f25078aa58d81ed9844b8af9f81ad2
Will Ware <wware@alum.mit.edu>
2015-08-14 16:37:05 -0400

    Accommodate both Linux and OSX

--- nextstage/swig/runswig.sh
+++ nextstage/swig/runswig.sh
@@ -16,8 +16,20 @@ then
 fi
 
 swig ${DEBUG} -c++ -python instrument.i || exit 1
-g++ -fPIC -c stub.cpp ../teensy/key.cpp instrument_wrap.cxx -I/usr/include/python2.7 -I../teensy || exit 1
-g++ -shared stub.o key.o instrument_wrap.o -o _instrument.so || exit 1
+g++ -fPIC -c stub.cpp ../teensy/key.cpp instrument_wrap.cxx -I/usr/include/python2.7 \
+    -I/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7/Python.h \
+    -I../teensy || exit 1
+
+if [ "$(uname -a | cut -c -6)" == "Darwin" ]
+then
+    # Mac
+    OPTS="-lpython -dynamiclib"
+else
+    # Linux
+    OPTS="-shared"
+fi
+
+g++ ${OPTS} stub.o key.o instrument_wrap.o -o _instrument.so || exit 1
 
 python testit.py || exit 1
 
