b884ebc85b284630422278b4972fa9e2ec5e7c38
Will Ware <wware@alum.mit.edu>
2015-08-19 23:45:58 -0400

    update the swig test

--- nextstage/swig/stub.cpp
+++ nextstage/swig/stub.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "common.h"
 #include "voice.h"
 #include "key.h"
@@ -11,4 +12,9 @@ uint8_t read_key(uint32_t id)
 {
     uint32_t Y = 0;
     return Y;
+}
+
+float small_random()
+{
+    return -2.0 + 0.01 * (rand() % 400);
 }
