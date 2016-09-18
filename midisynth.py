835f99ea92d2c1ddea94ac9f644e86bfbb159d5f
Will Ware <wware@alum.mit.edu>
2016-09-18 12:20:59 -0400

    Improvements to startup script.

--- midisynth.py
+++ midisynth.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 
+"""
+If running Ubuntu, add this line to the end of your
+$HOME/.profile.
+
+/usr/bin/python /home/wware/midisynth.py &
+
+Change "wware" to your own username, and copy this file
+into your home directory.
+"""
+
 import os
 import re
 import time
 
-assert os.system("fluidsynth --server --no-shell --audio-driver=alsa /usr/share/sounds/sf2/FluidR3_GM.sf2 &") == 0
-time.sleep(2)
+cmd = (
+    "fluidsynth --server --no-shell --audio-driver=alsa " +
+    "/usr/share/sounds/sf2/FluidR3_GM.sf2 &"
+)
+print cmd
+assert os.system(cmd) == 0
 
 R = os.popen('ps ax | grep fluidsynth | grep -v grep').read()
 if not R:
     os.system("ps ax | grep fluidsynth | cut -c -6 | xargs kill -9")
     assert R, R
 
+iters = 0
+while True:
+    time.sleep(1)
+    R = os.popen('aconnect -l').read()
+    if "FLUID Synth" in R:
+        r2 = re.compile(r"client (\d+): 'FLUID Synth").search(R)
+        fluid_num = r2.group(1)
+        break
+    iters += 1
+    assert iters < 15, "Cannot find fluidsynth process"
+
 connected = False
-R = os.popen('aconnect -l').read()
-r2 = re.compile(r"client (\d+): 'FLUID Synth").search(R)
-if not r2:
-    os.system("ps ax | grep fluidsynth | cut -c -6 | xargs kill -9")
-    assert r2, R
-
-fluid_num = r2.group(1)
-
 try:
     while True:
         time.sleep(1)
